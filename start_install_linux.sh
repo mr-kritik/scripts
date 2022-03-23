2b884ea2109e200ff4d78fb7e4a64107847014be
mr-kritik <V.Tegin@redbull18.ru>
2022-03-23 09:21:49 +0300

    Update

--- start_install_linux.sh
+++ start_install_linux.sh
@@ -24,3 +24,10 @@ rm -rf Docker_VZ
 git clone https://github.com/mr-kritik/Docker_VZ.git
 git clone https://github.com/mr-kritik/VZ.git
 python3 -m pip install -r $HOME/VZ/MHDDoS/requirements.txt
+
+
+# Install docker
+printf '%s\n' "deb https://download.docker.com/linux/debian bullseye stable" | sudo tee /etc/apt/sources.list.d/docker-ce.list
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-ce-archive-keyring.gpg
+sudo apt update
+apt install -y docker-ce docker-ce-cli containerd.io
