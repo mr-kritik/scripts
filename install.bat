8a42c7ded9dbe56a58962586bce30f5b3c0b544f
mr-kritik <89460465+mr-kritik@users.noreply.github.com>
2022-03-23 16:25:47 +0300

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,11 @@
+mkdir C:\z
+cd /d C:\z
+curl -O -o https://desktop.docker.com/win/main/amd64/Docker%%20Desktop%%20Installer.exe
+curl "https://www.python.org/ftp/python/3.10.2/python-3.10.2-amd64.exe" -o "python.exe"
+curl -L -O -o https://github.com/git-for-windows/git/releases/download/v2.35.1.windows.2/Git-2.35.1.2-64-bit.exe
+Docker%%20Desktop%%20Installer.exe install --quiet
+python.exe /S /install
+Git-2.35.1.2-64-bit.exe /silent
+cd /d C:\
+rd /s /q z
+pause
